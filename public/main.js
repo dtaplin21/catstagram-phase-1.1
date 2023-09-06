51691421f1aac3b1e07b157844ba30d86bd20d81
Dom <dtaplin21@aol.com>
2023-09-05 21:31:15 -0700

    cat

--- public/main.js
+++ public/main.js
@@ -1,4 +1,12 @@
-export const createMainContent = () => {
+const catButton = () => {
+    const button = document.createElement("button");
+    button.id = "new-cat";
+button.innerText = "New Cat";
+button.addEventListener("click", fetchImage);
+return button;
+}
+
+ export const createMainContent = () => {
     // Create h1
     const h1 = document.createElement("h1");
     h1.innerText = "Catstagram";
@@ -7,12 +15,16 @@ export const createMainContent = () => {
     const img = document.createElement("img");
     img.style.margin = "20px";
     img.style.maxWidth = "750px";
-
+    fetchImage();
+    const newCatButton = catButton();
+    const voting = createVote()
     const container = document.querySelector(".container");
     container.appendChild(h1);
+    container.append(newCatButton)
     container.appendChild(img);
+    container.appendChild(voting)
 
-    fetchImage();
+
 };
 
 const fetchImage = async () => {
@@ -28,3 +40,24 @@ const fetchImage = async () => {
         console.log("Failed to fetch image", e);
     }
 };
+
+function createVote() {
+    const buttonDiv = document.createElement("div");
+    const upVoteButton = document.createElement("button");
+    upVoteButton.id = "up-vote"
+    upVoteButton.innerText = "Up Vote";
+    upVoteButton.addEventListener("click", vote)
+
+    const downVoteButton = document.createElement("button");
+     downVoteButton.id = "down-vote"
+     downVoteButton.innerText = "down Vote";
+     downVoteButton.addEventListener("click", vote)
+
+     buttonDiv.appendChild(upVoteButton)
+ buttonDiv.appendChild(downVoteButton)
+ return buttonDiv
+}
+
+function vote() {
+
+}
