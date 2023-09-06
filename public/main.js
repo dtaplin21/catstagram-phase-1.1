aa01fb6ebddccd05f31a72832a47428a7dc481c7
Dom <dtaplin21@aol.com>
2023-09-05 21:59:42 -0700

    cats

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+
 const catButton = () => {
     const button = document.createElement("button");
     button.id = "new-cat";
@@ -19,10 +20,19 @@ return button;
     const newCatButton = catButton();
     const voting = createVote()
     const container = document.querySelector(".container");
+    const scoreContainer = document.createElement("div");
+    const scoreLabel = document.createElement("span");
+    scoreLabel.innerText = "Popularity Score: ";
+    const scoreSpan = document.createElement("span");
+    scoreSpan.className = "score";
+    scoreSpan.innerText = "0"
     container.appendChild(h1);
     container.append(newCatButton)
     container.appendChild(img);
     container.appendChild(voting)
+    container.appendChild(scoreContainer)
+    scoreContainer.appendChild(scoreLabel)
+    scoreContainer.appendChild(scoreSpan)
 
 
 };
@@ -48,16 +58,26 @@ function createVote() {
     upVoteButton.innerText = "Up Vote";
     upVoteButton.addEventListener("click", vote)
 
+
     const downVoteButton = document.createElement("button");
      downVoteButton.id = "down-vote"
-     downVoteButton.innerText = "down Vote";
+     downVoteButton.innerText = "Down Vote";
      downVoteButton.addEventListener("click", vote)
+
 
      buttonDiv.appendChild(upVoteButton)
  buttonDiv.appendChild(downVoteButton)
  return buttonDiv
 }
 
-function vote() {
 
+
+function vote(e) {
+   const score = document.querySelector(".score");
+   const newScore = score.innerText
+  if(e.target.id === "up-vote") {
+      score.innerText = parseInt(newScore) + 1
+  } else {
+    score.innerText = parseInt(newScore) - 1
+  }
 }
